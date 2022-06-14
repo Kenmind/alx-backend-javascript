decf986d87fb9c08533129f21c503836a7763365
Kenmind <kennedykalaluka04@gmail.com>
2022-06-14 11:38:41 +0200

    Const or let

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,16 @@
+// Instatiates a variable usin const
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+// Instatiates a variable using let
+export function taskNext() {
+  const combination = 'But sometimes let';
+  combination += getLast();
+  return combination;
+}
