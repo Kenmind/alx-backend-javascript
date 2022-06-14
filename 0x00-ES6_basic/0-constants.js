b598818702910f195165476a6f47fd5837eec219
Kennedy Kalaluka <62250484+Kenmind@users.noreply.github.com>
2022-06-14 11:48:54 +0200

    Update 0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,4 +1,3 @@
-// Instatiates a variable usin const
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
@@ -8,9 +7,9 @@ export function getLast() {
   return ' is okay';
 }
 
-// Instatiates a variable using let
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
+
   return combination;
 }
