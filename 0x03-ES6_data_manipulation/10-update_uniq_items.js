36c240b85a8c19afe931fbfda1eff298720bc9fe
Kenmind <kennedykalaluka04@gmail.com>
2022-06-21 14:22:20 +0200

    Log err msg

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,5 +1,5 @@
 export default function updateUniqueItems(myMap) {
-  if (Object.getPrototypeOf(myMap) !== Map.prototype) throw new TypeError('cannot process');
+  if (Object.getPrototypeOf(myMap) !== Map.prototype) throw new TypeError('Cannot process');
   myMap.forEach((quantity, name) => {
     if (quantity === 1) {
       myMap.set(name, 100);
