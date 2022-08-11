f6e9c0ef443864246db2aad0a5530e24ed4467eb
Kenmind <kennedykalaluka04@gmail.com>
2022-08-11 14:09:01 +0200

    test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,28 +1,28 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
+
 describe('calculateNumber', () => {
-  it("Checking floating whole numbers", () => {
+  it('checking floating whole numbers', () => {
     assert.strictEqual(calculateNumber(3.0, 4.0), 7);
   });
 
-  it("Checking rounding up numbers", () => {
+  it('checking rounding up numbers', () => {
     assert.strictEqual(calculateNumber(2.5, 3.709), 7);
   });
 
-  it("Checking rounding up numbers less than *.5", () => {
+  it('checking rounding up numbers less than *.5', () => {
     assert.strictEqual(calculateNumber(2.4, 5.24), 7);
   });
 
-  it("Checking whole numbers", () => {
+  it('checking whole numbers', () => {
     assert.strictEqual(calculateNumber(3, 4), 7);
   });
 
-  it("Checking negative numbers", () => {
+  it('checking negative numbers', () => {
     assert.strictEqual(calculateNumber(-1.3, -4.2), -5);
   });
 
-  it("Checking both negative and positive numbers", () => {
+  it('checking both negative and positive numbers', () => {
     assert.strictEqual(calculateNumber(-40, 50), 10);
   });
-
 });
